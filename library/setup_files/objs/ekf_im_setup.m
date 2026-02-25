fa8c6d9599a6d5e4796f2360bf267a10073c464a
davide.bagnara@leitner.com <davide.bagnara@leitner.com>
2026-02-25 11:45:07 +0100

    no message

--- library/setup_files/objs/ekf_im_setup.m
+++ library/setup_files/objs/ekf_im_setup.m
@@ -6,6 +6,7 @@
 
 classdef ekf_im_setup
     properties
+        A_tilde_ekf
         A1_tilde_ekf
         A2_tilde_ekf
         A3_tilde_ekf
@@ -34,7 +35,6 @@ classdef ekf_im_setup
                 obj.gamma = gamma;
                 obj.lm = lm;
                 obj.ts = ts;
-                
                 
                 obj.A1_tilde_ekf = [-gamma 0 beta*alpha 0 0 0; ...
                                     0 -gamma 0 beta*alpha 0 0; ...
@@ -69,20 +69,41 @@ classdef ekf_im_setup
                 obj.C_ekf = [1 0 0 0 0 0; 0 1 0 0 0 0];
                 obj.Bd_ekf = obj.B_tilde_ekf * obj.ts;
                 
+                obj.A_tilde_ekf = obj.A1_tilde_ekf + obj.A2_tilde_ekf + obj.A3_tilde_ekf + obj.A4_tilde_ekf;
                 %% Kalman init
-                Qkalman = [1 0 0 0 0 0; ...
-                               0 1 0 0 0 0; ...
-                               0 0 1 0 0 0;...
-                               0 0 0 1 0 0;...
+                Qkalman = [ts^2 0 0 0 0 0; ...
+                               0 ts^2 0 0 0 0; ...
+                               0 0 ts 0 0 0;...
+                               0 0 0 ts 0 0;...
                                0 0 0 0 1 0;...
                                0 0 0 0 0 1];
-                
+                % Qkalman = [1 0 0 0 0 0; ...
+                %                0 1 0 0 0 0; ...
+                %                0 0 1 0 0 0;...
+                %                0 0 0 1 0 0;...
+                %                0 0 0 0 ts 0;...
+                %                0 0 0 0 0 ts];                
                 obj.Qkalman = obj.ts * Qkalman;
-                obj.Rkalman = [2 0; 0 2];
+                % obj.Qkalman = Qkalman;
+                obj.Rkalman = obj.ts * [1 0; 0 1];
+                % obj.Rkalman = [1 0; 0 1];
                 
                 obj.k_kalman = 1;
                 obj.k_dc = 0;
                 obj.k_sw = 0;
+                
+                Co = ctrb(obj.A_tilde_ekf, obj.Qkalman);
+                if rank(Co) == size(obj.A_tilde_ekf,1)
+                    disp('EKF Fully controllable');
+                else
+                    disp('EKF Not fully controllable');
+                end
+
+                [P, L, G, report] = idare(obj.A_tilde_ekf', obj.C_ekf', obj.Qkalman, obj.Rkalman); 
+
+                if report.Report == 0
+                    disp('EKF seems stable.');
+                end
             end
         end
     end
