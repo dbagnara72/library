f5dbbbdb38ce9c1def94c429bab35b4a687345dc
dab <dbagnara@gmail.com>
2026-03-28 20:16:44 +0100

    no message

--- library/setup_files/simulink_models_initialization/psm_calculus.m
+++ library/setup_files/simulink_models_initialization/psm_calculus.m
@@ -10,6 +10,7 @@ function psm = psm_calculus()
     psm_Ld = 1.04e-3;
     psm_Jm = 900e3/6;
     
-    psm = pmsm_setup('WindGen', psm_sys, psm_pwr, psm_i, psm_rpm, psm_np, psm_eta, psm_Lq, psm_Ld, psm_Jm, 0, 0);
+    % psm = pmsm_setup(name, pwr_nom, i_nom, rpm_nom, number_poles, eta, Ld, Lq, Jm, h5_percent, h7_percent)
+    psm = pmsm_setup('WindGen', psm_sys, psm_pwr, psm_i, psm_rpm, psm_np, psm_eta, psm_Ld, psm_Lq, psm_Jm, 0, 0);
     displayInfo(psm);
 end
