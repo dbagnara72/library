509c6d84e4018c7b47732d48c758738b526239fc
davide.bagnara@leitner.com <davide.bagnara@leitner.com>
2026-08-19 07:42:44 +0200

    no message

---
+++ library/setup_files/semiconductor_devices_data/devices_source_dataname/abb_5SNA1000G650300.m
@@ -0,0 +1,35 @@
+
+%% 5SNA1000G650300 (IGBT)
+device_name = 'abb_5SNA1000G650300';
+device_type = 'Si-IGBT';
+
+Vth = 5.5;                                              % [V]
+Rce_on = 0.15e-3;                                       % [Ohm]
+Vce_sat = 2.15;                                         % [V]
+Vdon_diode = 1.75;                                      % [V]
+Rdon_diode = 0.15e-3;                                   % [Ohm]
+Eon = 400e-3;                                           % [J] @ Tj = 125°C
+Eoff = 400e-3;                                          % [J] @ Tj = 125°C
+Erec = 245e-3;                                          % [J] @ Tj = 125°C
+Voff_sw_losses = 900;                                   % [V]
+Ion_sw_losses = 1200;                                   % [A]
+JunctionTermalMass = 1.0;                               % [J/K]
+Rtim = 4.0/1000;                                        % [K/W]
+Rth_switch_JC = 24.3/1000;                              % [K/W]
+Rth_switch_CH = 19.6/1000;                              % [K/W]
+Rth_switch_JH = Rtim + Rth_switch_JC + Rth_switch_CH;   % [K/W]
+Rth_diode_JC = 43.9/1000;                               % [K/W]
+Rth_diode_CH = 22.9/1000;                               % [K/W]
+Rth_diode_JH = Rtim + Rth_diode_JC + Rth_diode_CH;      % [K/W]
+Lstray_module = 18e-9;                                  % [H]
+Irr = 1100;                                             % [A]
+Cies = 54e-9;                                           % [F]
+Cres = 1.7e-9;                                          % [F]
+Rgate_internal = 2.3;                                   % [F]
+td_on = 0.6e-6;                                         % [s]
+trise = 0.11e-6;                                        % [s]
+td_off = 1.25e-6;                                       % [s]
+tfall = 0.49e-6;                                        % [s]
+Csnubber = 12e-12;                                      % [F]
+Rsnubber = 2200;                                        % [Ohm]
+% ------------------------------------------------------------
